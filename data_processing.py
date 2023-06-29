c181e73e2355c10299d6681a7db16f197d87e944
HiedaRaku-Win <hiedaraku@hotmail.com>
2023-06-29 11:28:23 +0800

    再次重构代码

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,5 @@
+# data_processing.py
 import datetime
-import sqlite3
 import pytz
 
 # 创建时区对象（协调世界时）
@@ -23,53 +23,6 @@ field_mapping = {
     'sst': 'road_subsurface_temperature'
 }
 
-class DatabaseManager:
-    def __init__(self, db_file):
-        self.db_file = db_file
-        self.conn = None
-        self.cursor = None
-
-    def connect(self):
-        try:
-            self.conn = sqlite3.connect(self.db_file)
-            self.cursor = self.conn.cursor()
-        except sqlite3.Error as e:
-            print(f"Database error: {e}")
-            return None
-
-    def create_tables(self):
-        # 创建数据表的逻辑
-        create_table_query = '''
-        CREATE TABLE IF NOT EXISTS data (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            data_time TEXT,
-            air_temperature REAL,
-            dew_point REAL,
-            rainfall REAL,
-            snowfall REAL,
-            wind_speed REAL,
-            atmospheric_pressure REAL,
-            cloud_coverage INTEGER,
-            solar_flux REAL,
-            infrared_flux REAL,
-            precipitation INTEGER,
-            road_condition TEXT,
-            road_surface_temperature REAL,
-            road_subsurface_temperature REAL
-        );
-        '''
-        self.cursor.executescript(create_table_query)
-        self.conn.commit()
-
-    def commit(self):
-        self.conn.commit()
-
-    def close(self):
-        if self.cursor:
-            self.cursor.close()
-        if self.conn:
-            self.conn.close()
-
 def process_data(json_data, db_manager):
     # 提取所有可能的缩写
     all_abbreviations = list(field_mapping.keys())
@@ -90,4 +43,3 @@ def process_data(json_data, db_manager):
 
     # 执行插入操作
     db_manager.cursor.execute(insert_query, (formatted_time, *values[1:]))
-
